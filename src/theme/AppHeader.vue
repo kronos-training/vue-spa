6b11416392af100b0b4e2b441e1f9acc206dca06
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2018-01-03 19:45:02 -0500

    added NotFound and using route query and name

--- src/theme/AppHeader.vue
+++ src/theme/AppHeader.vue
@@ -6,8 +6,8 @@
       </router-link>
     </div>
     <div class="navbar-menu">
-      <router-link to="/category/front-end" class="navbar-item is-tab" exact>Front-end</router-link>
-      <router-link to="/category/mobile" class="navbar-item is-tab" exact>Mobile</router-link>
+      <router-link to="/category/front-end" class="navbar-item is-tab">Front-end</router-link>
+      <router-link :to="{ name: 'category', params: { id: 'mobile' }}" class="navbar-item is-tab">Mobile</router-link>
        <router-link to="/login" class="navbar-item is-tab">Login</router-link>
     </div>
   </nav>
