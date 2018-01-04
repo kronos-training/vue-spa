87e8bbb4f156acb874e4a374c7796bc6e73dac55
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2018-01-03 19:14:59 -0500

    using router-link

--- src/theme/AppHeader.vue
+++ src/theme/AppHeader.vue
@@ -1,9 +1,13 @@
 <template lang="html">
   <nav class="navbar has-shadow" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="/">
+      <router-link to="/" class="navbar-item" exact>
         <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
-      </a>
+      </router-link>
+    </div>
+    <div class="navbar-menu">
+      <router-link to="/" class="navbar-item is-tab" exact>Home</router-link>
+       <router-link to="/login" class="navbar-item is-tab">Login</router-link>
     </div>
   </nav>
 </template>
