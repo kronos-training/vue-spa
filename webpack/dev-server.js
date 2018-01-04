393634a38077c5f3e3930e0c14bafbfd0f8ee683
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2018-01-04 12:30:23 -0500

    added webpack.server.config and vue-server-renderer dependencie

--- webpack/dev-server.js
+++ webpack/dev-server.js
@@ -1,12 +1,11 @@
+const path = require('path')
 const webpack = require('webpack')
+const MFS = require('memory-fs')
 const clientConfig = require('./webpack.client.config')
+const serverConfig = require('/webpack.server.config')
 
-module.exports = function setupDevServer (app) {
+module.exports = function setupDevServer (app, onUpdate) {
   clientConfig.entry.app.unshift('webpack-hot-middleware/client')
-  // clientConfig.entry.app = [
-  //   'webpack-hot-middleware/client',
-  //   clientConfig.entry.app
-  // ];
 
   clientConfig.plugins.push(
     new webpack.HotModuleReplacementPlugin(),
@@ -24,4 +23,12 @@ module.exports = function setupDevServer (app) {
   )
 
   app.use(require('webpack-hot-middleware')(compiler))
+
+  const serverCompiler = webpack(serverConfig)
+  const mfs = new MFS()
+  const outputPath = path.join(serverConfig.output.path, 'server/main.js')
+  serverCompiler.outputFileSystem = mfs
+  serverCompiler.watch({}, () => {
+    onUpdate(mfs.readFileSync(outputPath, 'utf-8'))
+  })
 }
